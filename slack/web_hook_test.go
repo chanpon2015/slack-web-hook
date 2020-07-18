1e9c677f4aec12bb6bc139c512bb8ba46f7303b6
chanpon2015 <>
2020-07-18 16:16:13 +0900

    changed

--- slack/web_hook_test.go
+++ slack/web_hook_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestSend(t *testing.T) {
 	webHook := slack.WebHook{
-		URL: "https://hooks.slack.com/services/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",
+		URL: "",
 		Msg: slack.Message{},
 	}
 	actions := webHook.Msg.CreateActions()

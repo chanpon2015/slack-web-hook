a4cc2ba655bef8f548bd18c53c9f61db9ea0479e
chanpon2015 <>
2020-10-25 15:36:08 +0900

    up

---
+++ slack/webhook.go
@@ -0,0 +1,56 @@
+package slack
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+)
+
+// WebHook is
+type WebHook struct {
+	URL string
+	Msg Message
+}
+
+var httpClient http.Client
+
+func init() {
+	httpClient = *http.DefaultClient
+}
+
+// SetClient is
+func SetClient(client http.Client) {
+	httpClient = client
+}
+
+// Send is
+func (w *WebHook) Send() error {
+	msg, err := json.Marshal(w.Msg)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest(
+		http.MethodPost,
+		w.URL,
+		bytes.NewBuffer(msg),
+	)
+	if err != nil {
+		return err
+	}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		return errors.New(string(body))
+	}
+
+	return nil
+}
